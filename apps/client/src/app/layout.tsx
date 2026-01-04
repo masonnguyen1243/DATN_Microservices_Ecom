ae13ebdbdd5ef8352139f377f0fe92d28d7f295e
masonnguyen1243 <meowjcisgod@gmail.com>
2026-01-04 17:23:09 +0700

    chore: client base setup

---
+++ apps/client/src/app/layout.tsx
@@ -0,0 +1,38 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+import { ToastContainer } from "react-toastify";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Atino - Always Be Casual",
+  description: "Atino is the best clothing store for your fashion needs.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        <div className="mx-auto p-4 sm:px-0 sm:max-w-xl md:max-w-2xl lg:max-w-3xl xl:max-w-6xl">
+          {children}
+        </div>
+        <ToastContainer position="bottom-right" />
+      </body>
+    </html>
+  );
+}
---
+++ apps/client/src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="">
+      <h1 className="text-red-500">Day la test</h1>
+    </div>
+  );
+}
