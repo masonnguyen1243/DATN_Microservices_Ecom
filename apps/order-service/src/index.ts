743510b41590ad260abd70f729180dc94b756896
masonnguyen1243 <meowjcisgod@gmail.com>
2026-01-11 23:46:35 +0700

    feat: create order service

---
+++ apps/order-service/src/index.ts
@@ -0,0 +1,23 @@
+import Fastify from "fastify";
+
+const fastify = Fastify();
+
+fastify.get("/health", (request, reply) => {
+  return reply.status(200).send({
+    status: "Ok",
+    uptime: process.uptime(),
+    timeStamp: Date.now(),
+  });
+});
+
+const start = async () => {
+  try {
+    await fastify.listen({ port: 8001 });
+    console.log("Order service is running on port 8001!");
+  } catch (err) {
+    fastify.log.error(err);
+    process.exit(1);
+  }
+};
+
+start();
