b5b87490ca1147d8ba7aca89469d25963c738259
masonnguyen1243 <meowjcisgod@gmail.com>
2026-01-11 23:04:53 +0700

    feat: payment form with zod

--- packages/types/src/cart.ts
+++ packages/types/src/cart.ts
@@ -29,3 +29,20 @@ export const shippingFormSchema = z.object({
 });
 
 export type ShippingFormInputs = z.infer<typeof shippingFormSchema>;
+
+export const paymentFormSchema = z.object({
+  cardHolder: z.string().min(1, "Card holder is required"),
+  cardNumber: z
+    .string()
+    .min(16, "Card number must be 16 digits")
+    .max(16, "Card number must be 16 digits"),
+  expirationDate: z
+    .string()
+    .regex(
+      /^(0[1-9]|1[0-2])\/?([0-9]{4}|[0-9]{2})$/,
+      "Expiration date must be in MM/YY format"
+    ),
+  cvv: z.string().min(3, "CVV is required").max(3, "CVV must be 3 digits"),
+});
+
+export type PaymentFormInputs = z.infer<typeof paymentFormSchema>;
