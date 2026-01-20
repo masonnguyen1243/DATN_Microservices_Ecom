886d24f16d1b900275b41f5bfc6f6d102c790ebf
masonnguyen1243 <meowjcisgod@gmail.com>
2026-01-20 23:12:33 +0700

    feat: delete an product

--- apps/product-service/src/controllers/product.controller.ts
+++ apps/product-service/src/controllers/product.controller.ts
@@ -41,6 +41,15 @@ export const updateProduct = async (req: Request, res: Response) => {
   return res.status(200).json(updatedProduct);
 };
 
-export const deleteProduct = async (req: Request, res: Response) => {};
+export const deleteProduct = async (req: Request, res: Response) => {
+  const { id } = req.params;
+
+  const deletedProduct = await prisma.product.delete({
+    where: { id: Number(id) },
+  });
+
+  return res.status(200).json(deletedProduct);
+};
+
 export const getProducts = async (req: Request, res: Response) => {};
 export const getProduct = async (req: Request, res: Response) => {};
