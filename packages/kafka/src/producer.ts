64a422b0194e36cd2fef1e5ae2b8c8e7b2d87057
masonnguyen1243 <meowjcisgod@gmail.com>
2026-02-04 00:05:24 +0700

    feat: kafka client, producer, comsumer

---
+++ packages/kafka/src/producer.ts
@@ -0,0 +1,26 @@
+import { Kafka, Producer } from "kafkajs";
+
+export const createProducer = (kafka: Kafka) => {
+  const producer: Producer = kafka.producer();
+
+  const connect = async () => {
+    await producer.connect();
+    console.log("Kafka producer connected");
+  };
+
+  const send = async (topic: string, message: object) => {
+    await producer.send({
+      topic,
+      messages: [{ value: JSON.stringify(message) }],
+    });
+
+    console.log("Kafka send message");
+  };
+
+  const disconnect = async () => {
+    await producer.disconnect();
+    console.log("Kafka producer disconnected");
+  };
+
+  return { connect, send, disconnect };
+};
